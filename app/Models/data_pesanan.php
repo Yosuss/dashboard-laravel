d8197a96462f7ce25c22f7c476deebdf4aae4666
Argaaa63 <argaaaa53@gmail.com>
2024-10-08 19:41:57 +0700

    model field

---
+++ app/Models/data_pesanan.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class data_pesanan extends Model
+{
+    use HasFactory;
+    // Tentukan nama tabel
+    protected $table = 'data_pesanan';
+
+    // Jika primary key bukan 'id', tentukan nama primary key
+    protected $primaryKey = 'id_pesanan';
+
+    // Tentukan kolom-kolom yang dapat diisi
+    protected $fillable = [
+        'tanggal_pesanan',
+        'total_pesanan',
+    ];
+
+    // Jika tidak menggunakan timestamps, nonaktifkan
+    public $timestamps = true;
+}
